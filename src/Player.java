75df77da73551d5f48f405b477b2a517b6a54f94
BT1E10_18 <sample@bths.edu>
2022-05-11 12:20:21 -0400

    Initial commit

---
+++ src/Player.java
@@ -0,0 +1,9 @@
+public class Player {
+    private String character;
+    private String name;
+    private int health;
+    private int energy;
+    private int armour;
+    private int damage;
+
+}
